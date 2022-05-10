0b9eb23b0957561f6ec687ac815b5d8a3771db61
uninstall-lea <lea.bisson@gmail.com>
2022-05-10 14:09:16 +0200

    c plus beau

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: lbisson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 22:55:55 by lbisson           #+#    #+#             */
-/*   Updated: 2022/05/10 14:06:50 by lbisson          ###   ########.fr       */
+/*   Updated: 2022/05/10 14:08:51 by lbisson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100
-
 # endif
 
 # include <fcntl.h>
